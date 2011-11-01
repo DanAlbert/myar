815a82a4d2d5f03ee1cd1522a402b783505633cf
Dan Albert <dan@gingerhq.net>
2011-11-01 11:17:07 -0700

    Added doxygen headers to every file.
    Added license file.

--- main.c
+++ main.c
@@ -1,3 +1,35 @@
+/**
+ * @file main.c
+ * @author Dan Albert
+ * @date Created 10/17/2011
+ * @date Last updated 11/01/2011
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * @section DESCRIPTION
+ * 
+ * This program will illustrate the use of file I/O on UNIX by maintaining a
+ * UNIX archive library, in the standard archive format.
+ * 
+ * @see http://web.engr.oregonstate.edu/cgi-bin/cgiwrap/dmcgrath/classes/11F/cs311/index.cgi?file=project2
+ * 
+ */
 #define _BSD_SOURCE 1
 
 #include <assert.h>
